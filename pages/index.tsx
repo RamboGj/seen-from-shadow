6e9d34d4e945ca831ba9bf7b1321a29c9a6648fd
João Rambo <jpramboguanabara@hotmail.com>
2022-02-09 16:23:57 -0300

    search filter by chapter name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,54 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import Pagination  from '../components/Pagination'
+import { SearchIcon } from '@heroicons/react/outline'
 
 const chapters = [
-  {number: "Capítulo 1", image: '/thumb.png', href: "/author"},
-  {number: "Capítulo 2", image: '/thumb.png', href: "/author"},
-  {number: "Capítulo 3", image: '/thumb.png', href: "/author"},
-  {number: "Capítulo 4", image: '/thumb.png', href: "/author"},
-  {number: "Capítulo 5", image: '/thumb.png', href: "/author"},
-  {number: "Capítulo 6", image: '/thumb.png', href: "/author"},
+  {title: "Capítulo 1", image: '/thumb.png', href: "/author"},
+  {title: "Capítulo 2", image: '/thumb.png', href: "/author"},
+  {title: "Capítulo 3", image: '/thumb.png', href: "/author"},
+  {title: "Capítulo 4", image: '/thumb.png', href: "/author"},
+  {title: "Capítulo 5", image: '/thumb.png', href: "/author"},
+  {title: "Capítulo 6", image: '/thumb.png', href: "/author"},
 ]
 
 export default function Home() {
+  const [search, setSearch] = useState<string>('')
+
+  const lowerSearch = search.toLowerCase()
+
+  const filteredChapters = chapters.filter((chapter) => {
+    return chapter.title.toLowerCase().includes(lowerSearch)
+  })
+
   return (
     <>
-      <div className="w-full">
+      <div className="w-full bg-zinc-900 fixed">
+        <form method="GET" className='flex justify-center mx-auto mt-8'>
+          <div className="relative text-gray-600 focus-within:text-gray-400">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
+                <SearchIcon aria-hidden="true" className="w-6 h-6"/>
+              </button>
+            </span>
+            <input 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)} 
+            type="search" 
+            className="py-2 text-sm text-white bg-gray-900 rounded-lg border-gray-800 pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..."/>
+          </div>
+        </form>
         <div className="flex mx-auto items-center justify-center w-10/12 h-screen flex-wrap justify-between">
-          {chapters.map((chapter) => {
+          {filteredChapters.map((chapter) => {
             return (
-              <div className="cursor-pointer mt-16 mx-12">
+              <div key={chapter.title} className="cursor-pointer mt-16 mx-12">
                 <Link href={chapter.href}>
                   <img className="object-contain" src={chapter.image}/>
                 </Link>
-                <h1 className="text-lg mt-2 flex justify-center font-bold">{chapter.number}</h1>
+                <h1 className="text-lg text-white mt-2 flex justify-center font-bold">{chapter.title}</h1>
               </div>
             )
           })}
-          
         </div>
         <Pagination/>
       </div>
