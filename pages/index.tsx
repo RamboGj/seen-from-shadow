51e188ae80866060f2c1e0db4f3657c320eb3af7
João Rambo <jpramboguanabara@hotmail.com>
2022-02-09 13:43:17 -0300

    IMAGES GALLERY

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
 export default function Home() {
   return (
-      <h1>Hello world</h1>
+      <div className="w-full">
+        <div className="flex mx-auto items-center justify-center w-10/12 h-screen mt-24 flex-wrap justify-between">
+          <div className="mx-12">
+            <img className="object-contain" src="https://picsum.photos/300"/>
+          </div>
+          <div className="mx-12">
+           <img className="object-contain" src="https://picsum.photos/300"/>
+          </div>
+          <div className="mx-12">
+            <img className="object-contain" src="https://picsum.photos/300" />
+          </div>
+          <div className="mt-12 mx-12">
+            <img className="object-contain" src="https://picsum.photos/300"/>
+          </div>
+          <div className="mt-12 mx-12">
+            <img className="object-contain" src="https://picsum.photos/300"/>
+          </div>
+          <div className="mt-12 mx-12">
+            <img className="object-contain" src="https://picsum.photos/300"/>
+          </div>
+        </div>
+      </div>
   )
 }
