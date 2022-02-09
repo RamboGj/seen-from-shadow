48e96eaf78136f1b7e9adfffe6937c2ff8bf4b2d
João Rambo <jpramboguanabara@hotmail.com>
2022-02-09 15:05:16 -0300

    Pagination style components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,34 @@
+import Link from 'next/link'
+import Pagination  from '../components/Pagination'
+
+const chapters = [
+  {number: "Capítulo 1", image: '/thumb.png', href: "/author"},
+  {number: "Capítulo 2", image: '/thumb.png', href: "/author"},
+  {number: "Capítulo 3", image: '/thumb.png', href: "/author"},
+  {number: "Capítulo 4", image: '/thumb.png', href: "/author"},
+  {number: "Capítulo 5", image: '/thumb.png', href: "/author"},
+  {number: "Capítulo 6", image: '/thumb.png', href: "/author"},
+]
+
 export default function Home() {
   return (
+    <>
       <div className="w-full">
-        <div className="flex mx-auto items-center justify-center w-10/12 h-screen mt-24 flex-wrap justify-between">
-          <div className="mx-12">
-            <img className="object-contain" src="https://picsum.photos/300"/>
-          </div>
-          <div className="mx-12">
-           <img className="object-contain" src="https://picsum.photos/300"/>
-          </div>
-          <div className="mx-12">
-            <img className="object-contain" src="https://picsum.photos/300" />
-          </div>
-          <div className="mt-12 mx-12">
-            <img className="object-contain" src="https://picsum.photos/300"/>
-          </div>
-          <div className="mt-12 mx-12">
-            <img className="object-contain" src="https://picsum.photos/300"/>
-          </div>
-          <div className="mt-12 mx-12">
-            <img className="object-contain" src="https://picsum.photos/300"/>
-          </div>
+        <div className="flex mx-auto items-center justify-center w-10/12 h-screen flex-wrap justify-between">
+          {chapters.map((chapter) => {
+            return (
+              <div className="cursor-pointer mt-16 mx-12">
+                <Link href={chapter.href}>
+                  <img className="object-contain" src={chapter.image}/>
+                </Link>
+                <h1 className="text-lg mt-2 flex justify-center font-bold">{chapter.number}</h1>
+              </div>
+            )
+          })}
+          
         </div>
+        <Pagination/>
       </div>
+    </>
   )
 }
